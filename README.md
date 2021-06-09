120bdcdc47b24280e88a9069fc1da5ee3e5dcc12
Boomb0x <Ghost2130@gmail.com>
2021-06-09 00:14:35 -0400

    adds card builder to javascript, adds comments finishes readme, HTML changes

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-# Homework-4 (Work Day Planner)
+# Homework-6 (Weather_DashBoard)
 ## Links
 
-[Repository](https://github.com/Aladd616/Work-Day-Scheduler-ALL)
+[Repository](https://github.com/Aladd616/Weather_Dashboard_ALL)
 
-[Pages](https://aladd616.github.io/Work-Day-Scheduler-ALL/)
+[Pages](https://aladd616.github.io/Weather_Dashboard_ALL/)
 
 ## Description
 
-A webpage that shows a series of rows labeled with the 8 business hours.  the text fields are colored based upon what the current hour of the day is.  When text is entered into the text field and the save button is pressed it saves that text and displays it upon the page reloading.
+A webpage that shows PResents the user with a search bar prompting them to enter the name of a city.  Upon entering a city name and hitting the search button, the user is presented with the the name of the city and the current date as well as the current current days forecast for the city, including humidity, UV index Wind, and Humidity in a card at the top of the screen.  In the body of the page the user is presented with the 5 day forecast in the form of 5 cards with temperature, wind speed, and humidity each labeled with a date within the next 5 days.
 
-The color of the text fields is determined by comparing the value of the id attribute associated with each text field to the current hour stored in the variable hour, grey for past, red for current hour, and green for future.
+This page uses the OpenWeather API to obtain the weather data used in this app.  Two api calls are used in this app.  The first is used to obtain the latitude and longitude data for the entered city and these 2 pieces of information are used as parameters in the second api call in order to obtain the most accurate weather data.
 
-Upon pressing the save button the tet within the text area is saved within local storage labeled with the id attribute associated with the text field.  Upon page load this data is displayed within the text field labeled with the id attribute.
+
 
--- script.js
+++ script.js
@@ -1,15 +1,24 @@
+// initial variable declaration
 var cityFormEl = document.querySelector('#city-form');
 var cityInputEl = document.querySelector('#City');
 var todayBox = document.querySelector('#today-box');
 var dayList = document.querySelector('#day-list');
+var dayCityTitle = document.getElementById('today-date');
+var currentTemp = document.getElementById('today-temp');
+var currentHum = document.getElementById('today-hum');
+var currentWind = document.getElementById('today-wind');
+var currentuvi = document.getElementById('today-uv');
+var cardContainer = document.getElementById('5day-container');
+var weatherCards = document.getElementById('')
 
 var pastCity = []
 console.log('pastCity',pastCity);
 
-
+// stores the used city names in array in local storage
 var storedCity = JSON.parse(localStorage.getItem('Cities'));
 console.log('storedCity',storedCity);
 
+// function of the search button
 var cityForm = function (event){
     event.preventDefault();
 
@@ -19,16 +28,17 @@ var cityForm = function (event){
         pastCity.push(City);
         localStorage.setItem('Cities', JSON.stringify(pastCity));
         console.log(City);
-
-        // todayBox.textContent = '';
-        // cityInputEl.value = '';
+        dayCityTitle.textContent = City + " " +moment().format('l');
+        // removes created 5 day forecast cards if they r there already
+        cardContainer.innerHTML = "";
+        
     } else {
         alert('Please enter a city')
     }
 };
 
 
-
+// first api call primarily to obtain the lat and long data
 var getTodayWeather = function (city) {
     var todayApi = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=8066018655ed008933d2f7b865448329';
 
@@ -47,8 +57,9 @@ var getTodayWeather = function (city) {
                 })
             }
         })
-}
 
+}
+// second functino call to obtain the data used to disply
 var displayDayForecast = function(lat, long) {
     var forecastApi = 'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+long+'&units=imperial&exclude=hourly,minutely&appid=8066018655ed008933d2f7b865448329';
 
@@ -60,23 +71,78 @@ var displayDayForecast = function(lat, long) {
                 console.log(data);
 
         
-       var date = moment().format('l');
-       var currentTemp= data.current.temp;
-       var currentWind= data.current.wind_speed;
-       var currentHum= data.current.humidity;
-       var currentuvi= data.current.uvi;
+    //    grabs the data for the current days weather card
+       currentTemp.textContent = 'Temp: ' + data.current.temp;
+       currentWind.textContent = 'Wind: ' + data.current.wind_speed;
+       currentHum.textContent = 'Humidity: ' + data.current.humidity;
+       currentuvi.textContent = 'UV Index: ' + data.current.uvi;
 
-        
-       for (var i = 1; i < data.daily.length; i++) {
-            var dailyTemp = data.daily[i].temp.day;
-            var dailyWind= data.daily[i].wind_speed;
-            var dailyHum= data.daily[i].humidity;
+// changes the background color of the UV index section of the Current days weather card based on the UV vlaue
+       if (data.current.uvi <=2 ) {
+           currentuvi.classList = 'favorable'
+       }
+
+       else if (data.current.uvi <= 7 && data.currentuvi >= 3) {
+        currentuvi.classList = 'moderate'
+         }
+
+         if (data.current.uvi >= 8 ) {
+            currentuvi.classList = 'severe'
+        }
+                
+
+        // for loop to create the 5 day forecast cards
+       for (var i = 1; i < 6; i++) {
+        //    grabs the data from the api and creates the sections in the 5 day forecast cards
+            var dailyTemp = 'Temp: ' + data.daily[i].temp.day;
+            var dailyWind= 'Wind: ' + data.daily[i].wind_speed;
+            var dailyHum= 'Humidity: ' + data.daily[i].humidity;
             var futureDate = moment().add(i, 'days').format('l');
+
+
+           
+        // creates the cards and adds them to the HTML text
+             var card = document.createElement('div');
+             card.className = 'card weatherForecast';
+             
+             var cardBody = document.createElement('div');
+             cardBody.className = 'card-body';
+
+             var title = document.createElement('h5');
+             title.innerText = futureDate;
+             title.className = 'card-title';
+
+             var weatherList = document.createElement('ul')
+             weatherList.className = 'list-group list-group-flush'
+
+             var tempList = document.createElement('li');
+             tempList.className = 'list-group-item';
+             tempList.innerText = dailyTemp;
+
+             var windList = document.createElement('li');
+             windList.className = 'list-group-item';
+             windList.innerText = dailyWind;
+
+             var humList = document.createElement('li');
+             humList.className = 'list-group-item';
+             humList.innerText = dailyHum;
+
+             cardBody.appendChild(title);
+             cardBody.appendChild(weatherList);
+             weatherList.appendChild(tempList);
+             weatherList.appendChild(windList);
+             weatherList.appendChild(humList);
+             card.appendChild(cardBody);
+             cardContainer.appendChild(card);
+
+             
+
 
        }
             })
         }
     })
+    
 }
-
+// event listener for the first button
 cityFormEl.addEventListener('submit', cityForm);
